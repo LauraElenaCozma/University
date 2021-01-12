2a73ff0f0685e2bad806b2a0b741f9b061d08bd0
unknown <cozmalaura23@gmail.com>
2021-01-12 22:54:04 +0200

    Added sport project; CRUD operations

---
+++ DAW/Sport/Sport/Models/Proba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Sport.Models
+{
+    public class Proba
+    {
+        [Key]
+        public int IDProba { get; set; }
+        public String Denumire { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
