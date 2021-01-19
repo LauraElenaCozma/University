69bd8126d040698dbab4c788fe98781a7212584a
unknown <cozmalaura23@gmail.com>
2021-01-19 15:20:28 +0200

    Added Examen

---
+++ DAW/Examen/Examen/Models/TitluDenumireViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen.Models
+{
+    public class TitluDenumireViewModel
+    {
+        public String Titlu { get; set; }
+        public String Denumire { get; set; }
+    }
+}
